53502c1145273264f2b134b9914abeb2244bc932
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-07-06 12:58:11 -0400

    Disable no-else-return

--- copy_to_cwd.py
+++ copy_to_cwd.py
@@ -14,6 +14,15 @@ if (Path.cwd() / "pyproject.toml").exists():
     should_continue = input("pyproject.toml already exists. Overwrite? [y/N] ")
     if should_continue.lower() != "y":
         print("pyproject.toml not overwritten.")
-        exit(0)
-
-shutil.copy(script_dir / "pyproject.toml", Path.cwd() / "pyproject.toml")
+    else:
+        shutil.copy(script_dir / "pyproject.toml", Path.cwd() / "pyproject.toml")
+else:
+    shutil.copy(script_dir / "pyproject.toml", Path.cwd() / "pyproject.toml")
+if (Path.cwd() / ".clang-format").exists():
+    should_continue = input(".clang-format already exists. Overwrite? [y/N] ")
+    if should_continue.lower() != "y":
+        print(".clang-format not overwritten.")
+    else:
+        shutil.copy(script_dir / ".clang-format", Path.cwd() / ".clang-format")
+else:
+    shutil.copy(script_dir / ".clang-format", Path.cwd() / ".clang-format")
